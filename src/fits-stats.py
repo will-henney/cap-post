16a00652df026265942e7dc3a25878506e95dca4
William Henney <whenney@gmail.com>
2010-11-07 23:35:55 -0600

    New program fits-stats.py, calculates min and max of FITS file

---
+++ src/fits-stats.py
@@ -0,0 +1,21 @@
+"""
+Calculate various statistics of a FITS file
+"""
+import argparse
+import pyfits
+import scipy
+from scipy.ndimage.measurements import extrema
+
+parser = argparse.ArgumentParser(description="Calculate various statistics of a FITS file")
+parser.add_argument('file' help="FITS file name")
+args = parser.parse_args()
+
+data = pyfits.open(args.file)[0].data
+dmin, dmax, dminloc, dmaxloc = extrema(data)
+
+print "Minimum: %s at %s" % (dmin, dminloc)
+print "Maximum: %s at %s" % (dmax, dmaxloc)
+
+
+
+
