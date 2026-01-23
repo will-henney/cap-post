565b393914ec50330ba5e65d236bc2e7a2707c9b
William Henney <whenney@gmail.com>
2026-01-23 11:42:58 -0600

    Update compilation for Apple Silicon Macs
    Also reformatting with ruff

--- src/fits-stats.py
+++ src/fits-stats.py
@@ -1,21 +1,27 @@
+# /// script
+# requires-python = ">=3.8"
+# dependencies = [
+#     "astropy>=4.0",
+#     "scipy>=1.5",
+# ]
+# ///
 """
 Calculate various statistics of a FITS file
 """
-import argparse
-import pyfits
-import scipy
-from scipy.ndimage.measurements import extrema
 
-parser = argparse.ArgumentParser(description="Calculate various statistics of a FITS file")
-parser.add_argument('file', help="FITS file name")
+import argparse
+from astropy.io import fits as pyfits
+import scipy
+from scipy.ndimage import extrema
+
+parser = argparse.ArgumentParser(
+    description="Calculate various statistics of a FITS file"
+)
+parser.add_argument("file", help="FITS file name")
 args = parser.parse_args()
 
 data = pyfits.open(args.file)[0].data
 dmin, dmax, dminloc, dmaxloc = extrema(data)
 
-print "Minimum: %s at %s" % (dmin, dminloc)
-print "Maximum: %s at %s" % (dmax, dmaxloc)
-
-
-
-
+print("Minimum: %s at %s" % (dmin, dminloc))
+print("Maximum: %s at %s" % (dmax, dmaxloc))
